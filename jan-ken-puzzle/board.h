598c95145a198ada2e341db00227206a9944804f
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2018-09-06 00:26:12 -0300

    :herb: work in progress - backtrack solver

--- jan-ken-puzzle/board.h
+++ jan-ken-puzzle/board.h
@@ -3,8 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "macros.h"
 
+/* Bool */
+#define TRUE 1
+#define FALSE 0
+
+/* Pieces */
 #define ROCK 2
 #define PAPER 3
 #define SCISSORS 4
@@ -16,29 +20,24 @@ typedef struct board {
     int** content;
 } BOARD;
 
-BOARD *initBoard(int rows, int cols)
-{
+BOARD* initBoard(int rows, int cols) {
     int i, j;
     BOARD *b = (BOARD *)malloc(sizeof(BOARD));
     b->rows = rows;
     b->cols = cols;
     b->content = (int **)malloc(sizeof(int *));
-    for (i = 0; i < rows; i++)
-    {
+    for (i = 0; i < rows; i++) {
         b->content[i] = (int *)malloc(sizeof(int) * cols);
-        for (j = 0; j < cols; j++)
-        {
+        for (j = 0; j < cols; j++) {
             b->content[i][j] = EMPTY;
         }
     }
     return b;
 }
 
-BOARD *deleteBoard(BOARD *b)
-{
+BOARD* deleteBoard(BOARD *b) {
     int i;
-    for (i = 0; i < b->rows; i++)
-    {
+    for (i = 0; i < b->rows; i++) {
         free(b->content[i]);
     }
     free(b->content);
@@ -47,45 +46,56 @@ BOARD *deleteBoard(BOARD *b)
     return b;
 }
 
-void printBoard(BOARD *b)
-{
+void printBoard(BOARD *b, int x, int y) {
     int i, j;
 
     fprintf(stderr, "*");
-    for (i = 0; i < b->cols; i++)
-    {
-        fprintf(stderr, "***");
+    for (i = 0; i < b->cols; i++) {
+        fprintf(stderr, "******");
     }
     fprintf(stderr, "*\n");
 
-    for (i = 0; i < b->rows; i++)
-    {
+    for (i = 0; i < b->rows; i++) {
         fprintf(stderr, "*");
-        for (j = 0; j < b->cols; j++)
-        {
-            switch (b->content[i][j])
-            {
-            case ROCK:
-                fprintf(stderr, "[R]", b->content[i][j]);
-                break;
-            case PAPER:
-                fprintf(stderr, "[P]", b->content[i][j]);
-                break;
-            case SCISSORS:
-                fprintf(stderr, "[S]", b->content[i][j]);
-                break;
-            default:
-                fprintf(stderr, "[ ]", b->content[i][j]);
-                break;
+        for (j = 0; j < b->cols; j++) {
+            if (i == x && j == y) {
+                switch (b->content[i][j]) {
+                    case ROCK:
+                        fprintf(stderr, "[ R! ]");
+                        break;
+                    case PAPER:
+                        fprintf(stderr, "[ P! ]");
+                        break;
+                    case SCISSORS:
+                        fprintf(stderr, "[ S! ]");
+                        break;
+                    default:
+                        fprintf(stderr, "[  ! ]");
+                        break;
+                }
+            } else {
+                switch (b->content[i][j]) {
+                    case ROCK:
+                        fprintf(stderr, "[ R  ]");
+                        break;
+                    case PAPER:
+                        fprintf(stderr, "[ P  ]");
+                        break;
+                    case SCISSORS:
+                        fprintf(stderr, "[ S  ]");
+                        break;
+                    default:
+                        fprintf(stderr, "[    ]");
+                        break;
+                }
             }
         }
         fprintf(stderr, "*\n");
     }
 
     fprintf(stderr, "*");
-    for (i = 0; i < b->cols; i++)
-    {
-        fprintf(stderr, "***");
+    for (i = 0; i < b->cols; i++) {
+        fprintf(stderr, "******");
     }
     fprintf(stderr, "*\n");
 }
@@ -100,83 +110,73 @@ char gameOver(BOARD *b) {
     }
 }
 
-char checkMove(int a, int b)
-{
-    switch (a)
-    {
-    case ROCK:
-        switch (b)
-        {
+// a = current piece
+// b = neighbor
+char checkMove(int a, int b) {
+    switch (a) {
         case ROCK:
-            return FALSE;
+            switch (b) {
+                case ROCK:
+                    return FALSE;
+                    break;
+                case PAPER:
+                    return FALSE;
+                    break;
+                case SCISSORS:
+                    return TRUE;
+                    break;
+                case EMPTY:
+                    return FALSE;
+                    break;
+            }
             break;
+        
         case PAPER:
-            return FALSE;
+            switch (b) {
+                case ROCK:
+                    return TRUE;
+                    break;
+                case PAPER:
+                    return FALSE;
+                    break;
+                case SCISSORS:
+                    return FALSE;
+                    break;
+                case EMPTY:
+                    return FALSE;
+                    break;
+            }
             break;
+        
         case SCISSORS:
-            return TRUE;
-            break;
-        }
-        break;
-    case PAPER:
-        switch (b)
-        {
-        case ROCK:
-            return TRUE;
-            break;
-        case PAPER:
-            return FALSE;
-            break;
-        case SCISSORS:
-            return FALSE;
-            break;
-        }
-        break;
-
-    case SCISSORS:
-        switch (b)
-        {
-        case ROCK:
-            return FALSE;
-            break;
-        case PAPER:
-            return TRUE;
-            break;
-        case SCISSORS:
-            return FALSE;
-            break;
-        }
-        break;
-
-    default:
-        return TRUE;
+            switch (b) {
+                case ROCK:
+                    return FALSE;
+                    break;
+                case PAPER:
+                    return TRUE;
+                    break;
+                case SCISSORS:
+                    return FALSE;
+                    break;
+                case EMPTY:
+                    return FALSE;
+                    break;
+            }
         break;
     }
+    return FALSE;
 }
 
-// The objective of the game is to make valid moves until there is only one piece left.
-// There are three valid moves:
-
-// Move a rock piece into a neighbor cell, if the neighbor cell contains a scissors piece. The scissors piece is discarded;
-// Move a scissors piece into a neighbor cell, if the neighbor cell contains a paper piece. The paper piece is discarded; and
-// Move a paper piece into a neighbor cell, if the neighbor cell contains a rock piece. The rock piece is discarded.
-
-// if there is only n = 1 piece left, return indicating we found a solution,
-// otherwise, we did not;
-// otherwise, for each possible jump,
-// modify the board to account for the jump and call the backtracking algorithm recursively, where
-// if the algorithm indicates success, record the jump and return indicating we found a solution,
-// otherwise, reset the board to the state it was in prior to making the jump; and
-// the loop will only finish if none of the possible jumps led to a solution, so return indicating we did not find one.
-
-char solve(BOARD* b) {
+char solve(BOARD* b, int start_row, int start_col) {
     int i, j;
     int current, neighbor;
 
-    printf("\nCurrent board state:\n");
-    printBoard(b);
-    printf("Pieces left: %d.\n", b->pieces);
-    printf("\n");
+    fprintf(stderr, "\n** Solver entered [%d][%d]\n", start_row, start_col);
+    fprintf(stderr, "\nCurrent board state:\n");
+    printBoard(b, start_row, start_col);
+    fprintf(stderr, "Pieces left: %d.\n", b->pieces);
+    fprintf(stderr, "\n");
     
     // check if solution was found
     if (gameOver(b)) {
@@ -184,31 +184,31 @@ char solve(BOARD* b) {
     }
 
     // check all alive pieces for possible jumps
-    for (i = 0; i < b->rows; i++) {
-        for (j = 0; j < b->cols; j++) {
+    for (i = start_row; i < b->rows; i++) {
+        for (j = start_col; j < b->cols; j++) {
             current = b->content[i][j];
             if (current != EMPTY) {
-                printf("Trying all possible jumps for piece [%d][%d]\n", i, j);
                 // try all possible jumps
+                printBoard(b, start_row, start_col);
+                fprintf(stderr, "\nTrying all possible jumps for piece [%d][%d]\n\n", i, j);
+                
                 // UP
-                if (i-1 > 0) {
+                fprintf(stderr, "Checking UP...\n");
+                if (i-1 >= 0) {
                     neighbor = b->content[i-1][j];
-                    if (checkMove(current, neighbor))
-                    {
-                        printf("Jumping UP\n");
+                    if (checkMove(current, neighbor)) {
+                        fprintf(stderr, "\n* Jumping UP\n");
                         // execute jump
                         b->content[i-1][j] = current;
                         b->content[i][j] = EMPTY;
                         if (neighbor != EMPTY) b->pieces -= 1;
                         // recursive call
-                        if (solve(b))
-                        {
+                        if (solve(b, i-1, j) || solve(b, 0, 0)) {
                             return TRUE;
-                        }
-                        else
-                        {
+                        } else {
                             // reset the jump
-                            printf("Jump failed, resetting...\n");
+                            fprintf(stderr, "Jump failed, resetting...\n\n");
+                            printBoard(b, i, j);
                             b->content[i-1][j] = neighbor;
                             b->content[i][j] = current;
                             if (neighbor != EMPTY) b->pieces += 1;
@@ -217,26 +217,24 @@ char solve(BOARD* b) {
                 }
 
                 // DOWN
+                fprintf(stderr, "Checking DOWN...\n");
                 if (i+1 < b->rows) {
-                    neighbor = b->content[i + 1][j];
-                    if (checkMove(current, neighbor))
-                    {
-                        printf("Jumping DOWN\n");
+                    neighbor = b->content[i+1][j];
+                    if (checkMove(current, neighbor)) {
+                        fprintf(stderr, "\n* Jumping DOWN\n");
                         // execute jump
-                        b->content[i + 1][j] = current;
+                        b->content[i+1][j] = current;
                         b->content[i][j] = EMPTY;
                         if (neighbor != EMPTY) b->pieces -= 1;
             
                         // recursive call
-                        if (solve(b))
-                        {
+                        if (solve(b, i+1, j) || solve(b, 0, 0)) {
                             return TRUE;
-                        }
-                        else
-                        {
+                        } else {
                             // reset the jump
-                            printf("Jump failed, resetting...\n");
-                            b->content[i + 1][j] = neighbor;
+                            fprintf(stderr, "Jump failed, resetting...\n\n");
+                            printBoard(b, i, j);
+                            b->content[i+1][j] = neighbor;
                             b->content[i][j] = current;
                             if (neighbor != EMPTY) b->pieces += 1;
                         }
@@ -244,66 +242,117 @@ char solve(BOARD* b) {
                 }
 
                 // LEFT
-                if (j-1 > 0) {
-                    neighbor = b->content[i][j - 1];
-                    if (checkMove(current, neighbor))
-                    {
-                        printf("Jumping LEFT\n");
+                fprintf(stderr, "Checking LEFT...\n");
+                if (j-1 >= 0) {
+                    neighbor = b->content[i][j-1];
+                    if (checkMove(current, neighbor)) {
+                        fprintf(stderr, "\n* Jumping LEFT\n");
                         // execute jump
-                        b->content[i][j - 1] = current;
+                        b->content[i][j-1] = current;
                         b->content[i][j] = EMPTY;
                         if (neighbor != EMPTY) b->pieces -= 1;
 
                         // recursive call
-                        if (solve(b))
-                        {
+                        if (solve(b, i, j-1) || solve(b, 0, 0)) {
                             return TRUE;
-                        }
-                        else
-                        {
+                        } else {
                             // reset the jump
-                            printf("Jump failed, resetting...\n");
-                            b->content[i][j - 1] = neighbor;
+                            fprintf(stderr, "Jump failed, resetting...\n\n");
+                            printBoard(b, i, j);
+                            b->content[i][j-1] = neighbor;
+                            b->content[i][j] = current;
+                            if (neighbor != EMPTY) b->pieces += 1;
+                        }
+                    }
+                }
+
+                // RIGHT
+                fprintf(stderr, "Checking RIGHT...\n");
+                if (j+1 < b->cols) {
+                    neighbor = b->content[i][j+1];
+                    if (checkMove(current, neighbor)) {
+                        fprintf(stderr, "\n* Jumping RIGHT\n");
+                        // execute jump
+                        b->content[i][j+1] = current;
+                        b->content[i][j] = EMPTY;
+                        if (neighbor != EMPTY) b->pieces -= 1;
+
+                        // recursive call
+                        if (solve(b, i, j+1) || solve(b, 0, 0)) {
+                            return TRUE;
+                        } else {
+                            // reset the jump
+                            fprintf(stderr, "Jump failed, resetting...\n\n");
+                            printBoard(b, i, j);
+                            b->content[i][j+1] = neighbor;
                             b->content[i][j] = current;
                             if (neighbor != EMPTY) b->pieces += 1;
                         }
                     }
                 }
                 
-                // RIGHT
-                if (j+1 < b->cols) {
-                    neighbor = b->content[i][j + 1];
-                    if (checkMove(current, neighbor))
-                    {
-                        printf("Jumping RIGHT\n");
-                        // execute jump
-                        b->content[i][j + 1] = current;
-                        b->content[i][j] = EMPTY;
-                        if (neighbor != EMPTY) b->pieces -= 1;
-
-                        // recursive call
-                        if (solve(b))
-                        {
-                            return TRUE;
-                        }
-                        else
-                        {
-                            // reset the jump
-                            printf("Jump failed, resetting...\n");
-                            b->content[i][j + 1] = neighbor;
-                            b->content[i][j] = current;
-                            if (neighbor != EMPTY) b->pieces += 1;
-                        }
-                    }
-                }
                 // tried all possible jumps for this piece 
-                printf("This piece can't jump anywhere. Moving on...\n");
+                fprintf(stderr, "\nThis piece can't jump anywhere nor leads to a solution. Moving on...\n\n");
             }
         }
     }
     // tried all possible jumps for current board state, no solution was found
+    fprintf(stderr, "\nTried all possible movements for current alive pieces. Current state won't lead to a solution.\n");
     return FALSE;
 }
 
+BOARD* backupBoard(BOARD* b) {
+    int i, j;
+    BOARD* backup = initBoard(b->rows, b->cols);
+    for (i = 0; i < b->rows; i++) {
+        for (j = 0; j < b->cols; j++) {
+            backup->content[i][j] = b->content[i][j];
+        }
+    }
+    backup->pieces = b->pieces;
+    return backup;
+}
+
+BOARD* restoreBoard(BOARD* b, BOARD* backup) {
+    int i, j;
+    for (i = 0; i < b->rows; i++) {
+        for (j = 0; j < b->cols; j++) {
+            b->content[i][j] = backup->content[i][j];
+        }
+    }
+    b->pieces = backup->pieces;
+    return b;
+}
+
+char compareState(BOARD* s1, BOARD* s2) {
+    int i, j;
+    for (i = 0; i < s1->rows; i++) {
+        for (j = 0; j < s1->cols; j++) {
+            if (s1->content[i][j] != s2->content[i][j]) {
+                return FALSE;
+            }
+        }
+    }
+    return TRUE;
+}
+
+int convertPiece(int piece) {
+    int converted = 1;
+    switch(piece) {
+        case ROCK:
+            converted = 1;
+            break;
+        case SCISSORS:
+            converted = 2;
+            break;
+        case PAPER:
+            converted = 3;
+            break;
+        case EMPTY:
+            converted = 0;
+            break;
+    }
+    return converted;
+}
 
 #endif
--- jan-ken-puzzle/main.c
+++ jan-ken-puzzle/main.c
@@ -2,17 +2,31 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-
-#include "macros.h"
 #include "board.h"
 
 int main(int argc, char const *argv[]) {
-    int i, j;
+    // board input
     int R, C;
     int cell;
 
+    // helpers
+    int i, j;
+    char buffer[64];
+    int bytes; 
+
+    // read board from STDIN
     scanf("%d %d", &R, &C);
-    BOARD *b = initBoard(R, C);
+    
+    BOARD* b = initBoard(R, C);
+    BOARD* backup = initBoard(R, C);
+    
+    BOARD** solutions = NULL;
+    BOARD** unique_solutions = NULL;
+    int* final_states = NULL;
+
+    int n_solutions = 0;
+    int n_unique_solutions = 0;
+    char is_unique = TRUE;
 
     for (i = 0; i < R; i++) {
         for (j = 0; j < C; j++) {
@@ -37,11 +51,91 @@ int main(int argc, char const *argv[]) {
             b->content[i][j] = cell;
         }
     }
+    backup = backupBoard(b);
 
-    if (solve(b)) {
-        fprintf(stderr, "Solution found!\n");
-    } else {
-        fprintf(stderr, "Solution NOT found!\n");
+    // check for all possible solutions
+    for (i = 0; i < R; i++) {
+        for (j = 0; j < C; j++) {
+            if (b->content[i][j] != EMPTY) {
+                fprintf(stderr, "\n\n***** Searching for solution starting at [%d][%d] ***** \n", i, j);
+                if (solve(b, i, j)) {
+                    fprintf(stderr, "********** SOLUTION FOUND!\n");
+                    solutions = (BOARD**)realloc(solutions, sizeof(BOARD*) * n_solutions + 1);
+                    solutions[n_solutions] = backupBoard(b);
+                    n_solutions++; 
+                } else {
+                    fprintf(stderr, "\n********** Solution NOT found!\n");
+                }
+            }
+            // restore board
+            b = restoreBoard(b, backup);
+        }
+    }
+
+    // count unique solutions
+    unique_solutions = (BOARD**)malloc(sizeof(BOARD*) * n_solutions);
+    final_states = (int*)malloc(sizeof(int) * n_solutions);
+
+    unique_solutions[0] = backupBoard(solutions[0]);
+
+    for (i = 0; i < unique_solutions[0]->rows; i++) {
+        for (j = 0; j < unique_solutions[0]->cols; j++) {
+            if (unique_solutions[0]->content[i][j] != EMPTY) {
+                final_states[0] =  100 * (i+1) + 10 * (j+1) + convertPiece(unique_solutions[0]->content[i][j]);
+            }
+        }
+    }
+
+    n_unique_solutions = 1;
+
+    for (i = 0; i <= n_solutions-1; i++) {
+        is_unique = TRUE;
+        for (j = 0; j < n_unique_solutions; j++) {
+            if (compareState(solutions[i], unique_solutions[j])) {
+                is_unique = FALSE;
+            }
+        }
+        if (is_unique) {
+            // store unique solution
+            unique_solutions[n_unique_solutions] = backupBoard(solutions[i]);
+
+            // store final state as an integer (row, column, piece)
+            for (i = 0; i < unique_solutions[n_unique_solutions]->rows; i++) {
+                for (j = 0; j < unique_solutions[n_unique_solutions]->cols; j++) {
+                    if (unique_solutions[n_unique_solutions]->content[i][j] != EMPTY) {
+                        final_states[n_unique_solutions] =  100 * (i+1) + 10 * (j+1) + convertPiece(unique_solutions[n_unique_solutions]->content[i][j]);
+                    }
+                }
+            }
+            n_unique_solutions++;
+        }
+    }
+
+    fprintf(stderr, "\nNumber of ways to get to a solution: %d\n", n_solutions);
+    fprintf(stderr, "Number of distinct final states (solutions): %d\n\n", n_unique_solutions);
+
+    printf("%d\n", n_solutions);
+    printf("%d\n", n_unique_solutions);
+    
+    // sort unique final states
+    int tmp;
+    for (i = 1 ; i <= n_unique_solutions - 1; i++) {
+        j = i;
+        while (j > 0 && final_states[j-1] > final_states[j]) {
+            tmp = final_states[j];
+            final_states[j] = final_states[j-1];
+            final_states[j-1] = tmp;
+            j--;
+        }
+    }
+
+    // store state in a buffer so it can be printed correctly
+    for (i = 0; i < n_unique_solutions; i++) {
+        bytes = snprintf(buffer, sizeof(buffer), "%d", final_states[i]);
+        for (j = 0; j < bytes; j++) {
+            printf("%c ", buffer[j]);
+        }
+        printf("\n");
     }
 
     b = deleteBoard(b);
