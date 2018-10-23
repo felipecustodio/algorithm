a13666d61bfe6cf81c3342345d7ffc01d22bf62d
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2018-10-23 18:17:18 -0300

    :herb: tests for DP version

---
+++ jan-ken-puzzle-dp/tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+shopt -s nullglob
+
+for ext in .in; do
+    files=(test/*"$ext")
+    printf 'number of testfiles: %d\n' "${#files[@]}"
+
+    for i in `seq 1 ${#files[@]}`; do
+        rm results$i.out;
+        printf "\nRunning test $i\n";
+        ./puzzle < test/$i.in 2>&1 | tee test/results$i.out;
+        diff test/results$i.out test/$i.out;
+        printf ".....................\n";
+    done
+done
